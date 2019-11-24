80a733a9934a62b1ae0189594a1ecbf9c018a212
parthiban <parthiban@DESKTOP-VR5SBU2>
2019-11-24 19:08:00 +0530

    -------Day2Classroomwork

---
+++ src/main/java/day2/classroom/str/CharacterOccurance.java
@@ -0,0 +1,51 @@
+package day2.classroom.str;
+
+public class CharacterOccurance {
+
+
+	public static void main(String[] args) {
+
+		// Here is the input
+		String test = "I am getting better with Java";
+
+		// Here is what the count you need to find
+		char ch = 't';
+		int totalCount = 0;
+
+		char[] chArray =test.toCharArray();
+
+		for (int i=0; i <=chArray.length-1; i++)
+
+		{
+
+			if (chArray[i]==ch)
+
+			{
+				totalCount=totalCount+1;
+				System.out.println("Total count of character("+ch+"): " + totalCount);
+			}
+		}
+
+		// Build the logic to find the count of each
+		/* Pseudo Code:
+			a) Convert the String to character array
+			b) Traverse through each character (using loop)
+			c) If the given character matches the expected character (ch),
+			   Then, increment totalCount
+		 */
+
+
+
+
+
+
+
+
+
+
+		// Print the count here
+
+
+	}
+
+}
